483ac6ff2313421cd4c5e353f7a9a0d1f26052dd
Ashley Gao <gaoye0718@gmail.com>
2022-08-20 19:22:52 -0400

    WIP

--- main.py
+++ main.py
@@ -5,9 +5,44 @@ from sklearn.model_selection import train_test_split
 import torch_geometric.transforms as T
 
 from datasets import KarateDataset
+from models import Net
 
 dataset = KarateDataset()
 data = dataset[0]
 
-print(dataset)
-print(data)
+model = Net()
+
+torch.manual_seed(42)
+
+optimizer_name = "Adam"
+lr = 1e-1
+optimizer = getattr(torch.optim, optimizer_name)(model.parameters(), lr=lr)
+epochs = 200
+
+def train():
+  model.train()
+  optimizer.zero_grad()
+  F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+  optimizer.step()
+
+@torch.no_grad()
+def test():
+  model.eval()
+  logits = model()
+  mask1 = data['train_mask']
+  pred1 = logits[mask1].max(1)[1]
+  acc1 = pred1.eq(data.y[mask1]).sum().item() / mask1.sum().item()
+  mask = data['test_mask']
+  pred = logits[mask].max(1)[1]
+  acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+  return acc1,acc
+
+for epoch in range(1, epochs):
+  train()
+
+train_acc,test_acc = test()
+
+print('#' * 70)
+print('Train Accuracy: %s' %train_acc )
+print('Test Accuracy: %s' % test_acc)
+print('#' * 70)
---
+++ models/gcn.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+# GCN model with 2 layers
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(data.num_features, 16)
+        self.conv2 = GCNConv(16, int(data.num_classes))
+
+    def forward(self):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+data =  data.to(device)
+
+model = Net().to(device) 
