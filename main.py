c242cc5ee0acfc2b267539543d23fe9bf0b8798a
Ashley Gao <gaoye0718@gmail.com>
2022-08-20 18:59:07 -0400

    WIP

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import torch
 from torch_geometric.data import Data
 
-print(torch.cuda.is_available)
+edge_index = torch.tensor([[0, 1],
+                           [1, 0],
+                           [1, 2],
+                           [2, 1]], dtype=torch.long)
+x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
 
-x = torch.tensor([[2,1], [5,6], [3,7], [12,0]], dtype=torch.float)
-y = torch.tensor([0, 1, 0, 1], dtype=torch.float)
+data = Data(x=x, edge_index=edge_index.t().contiguous())
 
-edge_index = torch.tensor([[0, 1, 2, 0, 3],
-                           [1, 0, 1, 3, 2]], dtype=torch.long)
-
-data = Data(x=x, y=y, edge_index=edge_index)
+print(data)
