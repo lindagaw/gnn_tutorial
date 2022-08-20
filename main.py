e8db73db47ef7de48c509bc051e6e9598ae560bb
Ashley Gao <gaoye0718@gmail.com>
2022-08-20 18:44:50 -0400

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 from torch_geometric.data import Data
 
+print(torch.cuda.is_available)
+
 x = torch.tensor([[2,1], [5,6], [3,7], [12,0]], dtype=torch.float)
 y = torch.tensor([0, 1, 0, 1], dtype=torch.float)
 
