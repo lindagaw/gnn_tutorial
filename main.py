c4ca40fb34ca6051a7773685c4e13f5c528ad781
Ashley Gao <gaoye0718@gmail.com>
2022-08-21 10:07:41 -0400

    qip

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import imageio
 
-from torch_geometric.nn import GCNConv
+class GCNConv(nn.Module):
+    def __init__(self, A, in_channels, out_channels):
+        super(GCNConv, self).__init__()
+        self.A_hat = A+torch.eye(A.size(0))
+        self.D     = torch.diag(torch.sum(A,1))
+        self.D     = self.D.inverse().sqrt()
+        self.A_hat = torch.mm(torch.mm(self.D, self.A_hat), self.D)
+        self.W     = nn.Parameter(torch.rand(in_channels,out_channels))
+
+    def forward(self, X):
+        out = torch.relu(torch.mm(torch.mm(self.A_hat, X), self.W))
+        return out
 
 class Net(torch.nn.Module):
     def __init__(self,A, nfeat, nhid, nout):
